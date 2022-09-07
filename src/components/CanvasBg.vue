0511e1d8a5e276fdc2aaa2e37c72febb7e8a28a5
liuyue <1308972436@qq.com>
2022-09-08 00:14:42 +0800

    feat: 移动和放大

--- src/components/CanvasBg.vue
+++ src/components/CanvasBg.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, reactive, computed, watch } from 'vue'
+import { ref, onMounted, reactive, watch } from 'vue'
 import { fabric } from 'fabric'
 
 interface CanvasSize {
@@ -17,22 +17,18 @@ interface MousePosition {
     y: number
 }
 
+let canvas: fabric.Canvas
 const canvasEl = ref<HTMLCanvasElement>()
-const instance = ref<fabric.Canvas>()
-const bgImgSrc = new URL('../assets/Snipaste_2022-09-03_21-37-43.png', import.meta.url).href
-const bgimg = ref<fabric.Image>()
-const canvasOriginWidth = ref<number>(1920)
-const canvasOriginHeight = ref<number>(1080)
-const canvasScale = ref<number>(1)
+const bgImgSrc = new URL('../assets/Snipaste_2022-09-03_23-59-56.png', import.meta.url).href
 const isMoving = ref<boolean>(false)
 const container = ref<HTMLDivElement>()
+const canvasContainer = ref<HTMLDivElement>()
+const scale = ref<number>(1)
 let animationId: number
 
-const canvasSize = computed<CanvasSize>(() => {
-    return {
-        width: canvasOriginWidth.value * canvasScale.value,
-        height: canvasOriginHeight.value * canvasScale.value
-    }
+const canvasSize = reactive<CanvasSize>({
+    width: 800,
+    height: 750
 })
 
 const position = reactive<Position>({
@@ -47,93 +43,86 @@ const mousePosition = reactive<MousePosition>({
 
 onMounted(() => {
     init()
+    position.left = container.value?.offsetWidth! / 2
+    position.top = container.value?.offsetHeight! / 2
 })
 
 function init() {
-    instance.value = new fabric.Canvas(canvasEl.value!)
-
-    fabric.Image.fromURL(bgImgSrc, (imgElement) => {
-        bgimg.value = imgElement
-        canvasOriginWidth.value = imgElement.width!
-        canvasOriginHeight.value = imgElement.height!
-        instance.value?.setBackgroundImage(bgimg.value!, instance.value.renderAll.bind(instance.value), {
-            // scaleX: canvasSize.value.width / img.width,
-            // scaleY: canvasSize.value.height / img.height
+    canvas = new fabric.Canvas(canvasEl.value!)
+    fabric.Image.fromURL(bgImgSrc, (image) => {
+        const ratio = Math.min(container.value?.offsetWidth! * 0.8 / image.width!, container.value?.offsetHeight! * 0.8 / image.height!)
+        canvasSize.width = image.width! * ratio
+        canvasSize.height = image.height! * ratio
+        canvas.setBackgroundImage(image, canvas.renderAll.bind(canvas), {
+            width: image.width!,
+            height: image.height!,
+            scaleX: ratio,
+            scaleY: ratio
         })
-
-
-        instance.value?.renderAll()
+        canvas.renderAll()
     })
+
 }
 
 watch(canvasSize, (newVal) => {
-    console.log('newVal', newVal)
-    instance.value?.setWidth(newVal.width)
-    instance.value?.setHeight(newVal.height)
+    canvas.setWidth(newVal.width)
+    canvas.setHeight(newVal.height)
 })
 
-watch(canvasScale, () => {
-    fabric.Image.fromURL(bgImgSrc, (imgElement) => {
-        bgimg.value = imgElement
-        instance.value?.setBackgroundImage(bgimg.value!, instance.value.renderAll.bind(instance.value), {
-            scaleX: canvasScale.value,
-            scaleY: canvasScale.value
-        })
-        instance.value?.renderAll()
-    })
-})
 
 const mouseDown = (e: MouseEvent) => {
     if (!e.metaKey) return
-    instance.value!.selection = false
+    canvas.selection = false
     isMoving.value = true
-    instance.value?.setCursor('move')
+    canvas.setCursor('move')
     // 计算鼠标在元素中的坐标
-    mousePosition.x = e.pageX - container.value!.offsetLeft
-    mousePosition.y = e.pageY - container.value!.offsetTop
+    mousePosition.x = e.pageX - canvasContainer.value!.offsetLeft
+    mousePosition.y = e.pageY - canvasContainer.value!.offsetTop
 }
 
 const mouseMove = (e: MouseEvent) => {
-    if (!isMoving.value) return
-    instance.value?.setCursor('move')
-    const step = () => {
-        animationId = requestAnimationFrame(() => {
-            position.left = e.pageX - mousePosition.x
-            position.top = e.pageY - mousePosition.y
-        })
-    }
-    step()
+    if (!isMoving.value || !e.metaKey) return
+    if (e.pageX === mousePosition.x && e.pageY - mousePosition.y) return
+    position.left = e.pageX - mousePosition.x
+    position.top = e.pageY - mousePosition.y
 }
 
 const mouseUp = () => {
     if (!isMoving.value) {
         return
     }
-    instance.value?.setCursor('default')
+    canvas.setCursor('default')
     isMoving.value = false
     cancelAnimationFrame(animationId)
-    instance.value!.selection = true
+    canvas.selection = true
 }
 
 </script>
 
 <template>
-
     <div class="flex">
-        <div class="relative flex-1 h-80vh border-2 border-dashed border-black bg-gray-200">
-            <div class="absolute -translate-x-1/2 -translate-y-1/2"
-                :style="{ left: `${position.left}px`, top: `${position.top}px` }" @mousedown="mouseDown" ref="container"
-                @mousemove="mouseMove" @mouseup="mouseUp">
+        <div class="relative flex-1 h-100vh border-2 border-dashed border-black bg-gray-200 overflow-x-hidden overflow-y-scroll"
+            ref="container">
+            <div class="absolute "
+                :style="{ left: `${position.left}px`, top: `${position.top}px`, transform: `translate(-50%,-50%) scale(${scale})` }"
+                @mousedown="mouseDown" @mousemove="mouseMove" @mouseup="mouseUp" ref="canvasContainer">
                 <canvas ref="canvasEl" :width="canvasSize.width" :height="canvasSize.height"></canvas>
             </div>
         </div>
         <div class="w-200px">
-            <button class="mr-10" @click="canvasScale *= 1.25">+</button>
-            <button @click="canvasScale /= 1.25">-</button>
+            <div class="w-30px shadow p-16px mt-10px ml-10px">
+                <div class="i-mdi-magnify-plus text-25px mb-10px cursor-pointer hover:text-blue" @click="scale += 0.1">
+                </div>
+                <div class="mb-10px">{{ (scale * 100).toFixed(0) }}%</div>
+                <div class="i-mdi-magnify-minus  text-25px mb-10px cursor-pointer hover:text-blue"
+                    @click="scale -= 0.1"></div>
+                <div type="button" class="i-mdi-cursor-move text-25px mb-10px cursor-pointer hover:text-blue"
+                    :class="[isMoving ? 'text-blue' : '']" @click="isMoving = !isMoving">
+                </div>
+
+            </div>
         </div>
     </div>
-
-
 </template>
 
 <style scoped>
